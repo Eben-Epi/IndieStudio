70e6255cde834a2d516868c86e823f79ab008070
iElden <mceldenn@gmail.com>
2019-05-27 17:20:41 +0200

    add config file

---
+++ src/config.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** bomberman
+** File description:
+** congif.hpp
+*/
+
+#ifndef BOMBERMAN_CONGIF_HPP
+#define BOMBERMAN_CONGIF_HPP
+
+#define FRAME_RATE 60
+
+#define FRAME_TIME 1 / FRAME_RATE
+
+#endif //BOMBERMAN_CONGIF_HPP
