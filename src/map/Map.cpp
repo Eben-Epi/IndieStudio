2b0bee02051462c1910b106a574b181c57dd6d4f
Jonathan Martin <jonathan.martin@epitech.eu>
2019-05-28 11:51:35 +0200

    CMakeLists.txt changed to add Map.cpp Map.hpp

---
+++ src/map/Map.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2019
+** OOP_indie_studio_2018
+** File description:
+** Map.cpp
+*/
+
+#include "Map.hpp"
---
+++ src/map/Map.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2019
+** OOP_indie_studio_2018
+** File description:
+** Map.hpp
+*/
+
+#ifndef INDIESTUDIO_MAP_HPP
+#define INDIESTUDIO_MAP_HPP
+
+
+class Map {
+
+private:
+    ECS::ECSCore _map;
+
+public:
+
+};
+
+
+#endif //INDIESTUDIO_MAP_HPP
