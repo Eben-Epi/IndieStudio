9646cc5fe3574c04a799cd5fc8c50f4f35fdc041
iElden <mceldenn@gmail.com>
2019-05-28 11:53:55 +0200

    Added HealthSystem

---
+++ src/ecs/Systems/HealthSystem.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2019
+** bomberman
+** File description:
+** HealthSystem.cpp
+*/
+#include "HealthSystem.hpp"
+
+void ECS::HealthSystem::updateEntity(ECS::Entity &entity)
+{
+}
---
+++ src/ecs/Systems/HealthSystem.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2019
+** bomberman
+** File description:
+** HealthSystem.hpp
+*/
+
+#ifndef BOMBERMAN_HEALTHSYSTEM_HPP
+#define BOMBERMAN_HEALTHSYSTEM_HPP
+
+#include "../System.hpp"
+
+namespace ECS {
+    class HealthSystem : public System {
+
+    public:
+        void updateEntity(Entity &entity) override;
+    };
+}
+
+
+#endif //BOMBERMAN_HEALTHSYSTEM_HPP
