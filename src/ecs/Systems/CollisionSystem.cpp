a5ddb18d896ba0b160a3eb7ae7d5b9e31e7a56b2
Andgel <andgel.halley@epitech.eu>
2019-05-29 15:27:58 +0200

    Added CollisionSystem

--- src/ecs/Systems/CollisionSystem.cpp
+++ src/ecs/Systems/CollisionSystem.cpp
@@ -6,10 +6,29 @@
 */
 
 #include "CollisionSystem.hpp"
+#include "../ECSCore.hpp"
+#include "../Components/CollisionComponent.hpp"
+#include "../Components/DisplayableComponent.hpp"
 
 ECS::CollisionSystem::CollisionSystem(ECS::ECSCore &core):
     System("Collision", core)
-{}
+{
+	this->_dependencies = {"Displayable"};
+}
 
 void ECS::CollisionSystem::updateEntity(ECS::Entity &entity)
-{}
+{
+	auto &col = reinterpret_cast<CollisionComponent &>(entity.getComponentByName("Collision"));
+	auto &disp = reinterpret_cast<DisplayableComponent &>(entity.getComponentByName("Displayable"));
+
+	col.entitiesCollided = {};
+	for (auto &ent : this->_core.getEntitiesByComponent("Collision")) {
+		if (ent->getId() == entity.getId())
+			continue;
+
+		auto &disp2 = reinterpret_cast<DisplayableComponent &>(ent->getComponentByName("Displayable"));
+
+		if (disp.screen.areColliding(disp.entityId, disp2.entityId))
+			col.entitiesCollided.push_back(&*ent);
+	}
+}
