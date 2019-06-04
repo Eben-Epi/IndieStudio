2a984a2076e1f4bae371fccb1fd39ef6bcf721e3
Hégron Stanislas <Zargith@localhost.localdomain>
2019-06-04 19:00:18 +0200

    [BombDropperSystem.cpp] Managed removing old bombs from vector of Entities (bomb) of the component.

--- src/ecs/Systems/BombDropperSystem.cpp
+++ src/ecs/Systems/BombDropperSystem.cpp
@@ -6,6 +6,7 @@
 */
 #include "BombDropperSystem.hpp"
 #include "../ECSCore.hpp"
+#include "../Components/HealthComponent.hpp"
 
 ECS::BombDropperSystem::BombDropperSystem(ECS::ECSCore &core):
     System("BombDropper", core)
@@ -13,9 +14,14 @@ ECS::BombDropperSystem::BombDropperSystem(ECS::ECSCore &core):
 
 void ECS::BombDropperSystem::updateEntity(ECS::Entity &entity)
 {
-  ECS::BombDropperComponent &bomb = reinterpret_cast<ECS::BombDropperComponent &>(entity.getComponentByName("BombDropper"));
-  if (bomb.max >= bomb.bombs.size())
-    return;
-  else
-    bomb.bombs.push_back(reinterpret_cast<ECS::Entity &>(this->_core.makeEntity("Bomb")));
+    ECS::BombDropperComponent &bomb = reinterpret_cast<ECS::BombDropperComponent &>(entity.getComponentByName("BombDropper"));
+    for (size_t i = 0; i != bomb.bombs.size(); i++) {
+      auto &health = reinterpret_cast<ECS::HealthComponent &>(bomb.bombs[i].getComponentByName("Health"));
+      if (health.health == 0)
+        bomb.bombs.erase(bomb.bombs.begin() + i);
+    }
+    if (bomb.max >= bomb.bombs.size())
+      return;
+    else
+      bomb.bombs.push_back(reinterpret_cast<ECS::Entity &>(this->_core.makeEntity("Bomb")));
 }
