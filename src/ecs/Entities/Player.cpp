6b520477d3a0f57bae01e5b3f2ca5eee6542535e
iElden <mceldenn@gmail.com>
2019-06-04 12:21:13 +0200

    Finished BlockedSystem

--- src/ecs/Entities/Player.cpp
+++ src/ecs/Entities/Player.cpp
@@ -19,13 +19,13 @@
 ECS::Player::Player(unsigned id, const Ressources &ressources) :
 	Entity(id, "Player", {
 		new ControllableComponent(*ressources.inputs.at(id)),
-		new DisplayableComponent("Player", ressources),
 		new MovableComponent(5),
-		new PositionComponent({0, 0}, {TILESIZE - TILESIZE / 8, TILESIZE - TILESIZE / 8}),
-		new HealthComponent(1),
-		new CollisionComponent(0),
-		new ColliderComponent(0),
-		new BlockedComponent()
-	})
+        new PositionComponent({0, 0}, {TILESIZE - TILESIZE / 8, TILESIZE - TILESIZE / 8}),
+        new HealthComponent(1),
+        new CollisionComponent(0),
+        new ColliderComponent(0),
+        new BlockedComponent(),
+        new DisplayableComponent("Player", ressources)
+    })
 {
 }
--- src/ecs/Systems/BlockedSystem.cpp
+++ src/ecs/Systems/BlockedSystem.cpp
@@ -27,55 +27,57 @@ void ECS::BlockedSystem::updateEntity(ECS::Entity &entity)
         auto &entity_pos = reinterpret_cast<PositionComponent &>(entity.getComponentByName("Position"));
         auto &entity_mov = reinterpret_cast<MovableComponent &>(entity.getComponentByName("Movable"));
         auto &wall_hitbox = reinterpret_cast<ColliderComponent &>(wall->getComponentByName("Collider"));
+        auto &wall_pos = reinterpret_cast<PositionComponent &>(wall->getComponentByName("Position"));
 
         if (wall_hitbox.hardness <= entity_hitbox.passThrough)
             continue;
 
-        double relative_x = fmod(entity_pos.pos.x, TILESIZE);
-        double relative_y = fmod(entity_pos.pos.y, TILESIZE);
-        printf("[DEBUG] Collision at: x: %f y: %f rx: %f ry: %f TILESET: %d d: %d\n", entity_pos.pos.x, entity_pos.pos.y, relative_x, relative_y, TILESIZE, entity_mov.dir);
-        char block_id = (((static_cast<int>(relative_y) + PLAYERSIZE) / ((32 + PLAYERSIZE) / 4)) << 2)
-                        + (static_cast<int>(relative_x)) / FIRST_QUARTILE(0, TILESIZE);
-        printf("[DEBUG] block_id = %d (%d << 2 + %d)\n", block_id, ((static_cast<int>(relative_y) + PLAYERSIZE) / ((32 + PLAYERSIZE) / 4)), (static_cast<int>(relative_x) / FIRST_QUARTILE(0, TILESIZE)));
+        double relative_x = entity_pos.pos.x - wall_pos.pos.x;
+        double relative_y = entity_pos.pos.y - wall_pos.pos.y;
+        if (relative_x < -28.f || relative_x >= 32.f || relative_y < -28.f || relative_y >= 32.f)
+            continue;
+        char block_id = (static_cast<int>((4*(relative_y - -PLAYERSIZE) / (TILESIZE - -PLAYERSIZE))) << 2)
+                        + (static_cast<int>((4*(relative_x - -PLAYERSIZE) / (TILESIZE - -PLAYERSIZE))));
         switch (block_id) {
         case 0:
-        case 5:
-            entity_pos.pos.y -= MIN(relative_x, relative_y);
-            entity_pos.pos.x -= MIN(relative_x, relative_y);
+        case 5: // UP LEFT
+            entity_pos.pos.y -= MIN(relative_x + PLAYERSIZE, relative_y + PLAYERSIZE) + .1; // rup
+            entity_pos.pos.x -= MIN(relative_x + PLAYERSIZE, relative_y + PLAYERSIZE) + .1; // rleft
             break;
         case 1:
-        case 2:
-            entity_pos.pos.y -= relative_y;
-            entity_pos.pos.x += TILESIZE - MAX(relative_x, relative_y);
+        case 2: // UP
+            entity_pos.pos.y = wall_pos.pos.y - PLAYERSIZE - .1;
             break;
         case 3:
-        case 6:
-            entity_pos.pos.y -= MIN(relative_x, relative_y);
+        case 6: // UP RIGHT
+            entity_pos.pos.y -= MIN(relative_x + PLAYERSIZE, relative_y + PLAYERSIZE) + .1; // rup
+            entity_pos.pos.x += MIN(TILESIZE - relative_x, TILESIZE - relative_y) + .1; // rright
             break;
         case 4:
-        case 8:
-            entity_pos.pos.x -= relative_x;
+        case 8: // LEFT
+            entity_pos.pos.x = wall_pos.pos.x - wall_pos.size.x - .1;
             break;
         case 7:
-        case 11:
-            entity_pos.pos.x += TILESIZE - relative_x;
+        case 11: // RIGHT
+            entity_pos.pos.x = wall_pos.pos.x + wall_pos.size.x + .1;
             break;
         case 9:
-        case 12:
-            entity_pos.pos.y += PLAYERSIZE - MAX(relative_x, relative_y);
-            entity_pos.pos.x -= MIN(relative_x, relative_y);
+        case 12: // DOWN LEFT
+            entity_pos.pos.y += MIN(TILESIZE - relative_x, TILESIZE - relative_y) + .1; // rdown
+            entity_pos.pos.x -= MIN(relative_x + PLAYERSIZE, relative_y + PLAYERSIZE) + .1; // rleft
             break;
         case 13:
-        case 14:
-            entity_pos.pos.y += PLAYERSIZE - relative_y;
+        case 14: // DOWN
+            entity_pos.pos.y = wall_pos.pos.y + wall_pos.size.y + .1;
             break;
         case 10:
-        case 15:
-            entity_pos.pos.y += PLAYERSIZE - MAX(relative_x, relative_y);
-            entity_pos.pos.x += TILESIZE - MAX(relative_x, relative_y);
+        case 15: // DOWN RIGHT
+            entity_pos.pos.y += MIN(TILESIZE - relative_x, TILESIZE - relative_y) + .1; // rdown
+            entity_pos.pos.x += MIN(TILESIZE - relative_x, TILESIZE - relative_y) + .1; // rright
             break;
         default:
-            throw ECSException("[BlockedSystem] The impossible Occurred: block_id must be >= 0 and <= 15 but it was " + std::to_string(block_id));
+            dprintf(2, "[BlockedSystem] The impossible Occurred: block_id must be >= 0 and <= 15 but it was %d\n", block_id);
+            //throw ECSException("[BlockedSystem] The impossible Occurred: block_id must be >= 0 and <= 15 but it was " + std::to_string(block_id));
         }
     }
 }
