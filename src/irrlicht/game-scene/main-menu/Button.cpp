83915f88819c7cf91195b3cebfbbffec503f651b
Clément HUMBLOT <clement.humblot@epitech.eu>
2019-06-13 16:51:13 +0200

    Updated button and CmakeList

--- src/irrlicht/game-scene/main-menu/Button.cpp
+++ src/irrlicht/game-scene/main-menu/Button.cpp
@@ -5,3 +5,59 @@
 ** Button.cpp
 */
 
+#include "Button.hpp"
+
+Irrlicht::Button::Button(ECS::Point pos, ECS::Vector2<unsigned int> size, unsigned id, std::string text) :
+    id(id),
+    _pos(pos),
+    _size(size)
+{
+    if (!text.empty()) {
+        this->_text = text;
+        this->_button = this->_guienv->addButton(irr::core::rect<irr::s32>((int)_pos.x, (int)_pos.y, _size.x, _size.y), 0, this->id, reinterpret_cast<const wchar_t *>(&this->_text));
+    }
+    this->_button = this->_guienv->addButton(irr::core::rect<irr::s32>((int)_pos.x, (int)_pos.y, _size.x, _size.y), 0, this->id);
+}
+
+const std::string &Irrlicht::Button::getText() {
+    return this->_text;
+}
+
+const ECS::Point &Irrlicht::Button::getPos() {
+    return this->_pos;
+}
+
+const ECS::Vector2<unsigned> &Irrlicht::Button::getSize() {
+    return this->_size;
+}
+
+void Irrlicht::Button::setText(std::string text) {
+    delete this->_button;
+    this->_text = text;
+    if (!text.empty())
+        this->_button = this->_guienv->addButton(irr::core::rect<irr::s32>((int)_pos.x, (int)_pos.y, _size.x, _size.y), 0, this->id, reinterpret_cast<const wchar_t *>(&this->_text));
+    this->_button = this->_guienv->addButton(irr::core::rect<irr::s32>((int)_pos.x, (int)_pos.y, _size.x, _size.y), 0, this->id);
+}
+
+void Irrlicht::Button::setPos(ECS::Point pos) {
+    if (pos.x != _pos.x && pos.y != pos.y) {
+        this->_pos = pos;
+        this->_button->setRelativePosition(irr::core::position2di((int)pos.x, (int)pos.y));
+    }
+}
+
+void Irrlicht::Button::setSize(ECS::Vector2<unsigned> size) {
+    delete this->_button;
+    if (size.x != _size.x && size.y != _size.y) {
+        this->_size = size;
+        this->_button = this->_guienv->addButton(
+                irr::core::rect<irr::s32>((int) _pos.x, (int) _pos.y, _size.x, _size.y), 0, this->id,
+                reinterpret_cast<const wchar_t *>(&this->_text));
+    }
+}
+
+bool Irrlicht::Button::isPressed() {
+    return this->_button->isPressed();
+}
+
+Irrlicht::Button::~Button() = default;
--- src/irrlicht/game-scene/main-menu/Button.hpp
+++ src/irrlicht/game-scene/main-menu/Button.hpp
@@ -9,16 +9,31 @@
 #define BUTTON_HPP
 
 #include "../../../ecs/data/Vector2.hpp"
+#include <irrlicht/irrlicht.h>
+#include <string>
+#include <irrlicht/IGUIEnvironment.h>
 
 namespace Irrlicht
 {
 	class Button {
-		Button(ECS::Point pos, ECS::Vector2<unsigned> size);
+		Button(ECS::Point pos, ECS::Vector2<unsigned> size, unsigned id, std::string text = "");
 		~Button();
 
+		const std::string &getText();
+		const ECS::Point &getPos();
+		const ECS::Vector2<unsigned> &getSize();
+		void setText(std::string text);
+		void setPos(ECS::Point pos);
+		void setSize(ECS::Vector2<unsigned> size);
+		bool isPressed();
+
+		unsigned id;
 	private:
+	    std::string _text;
 		ECS::Point _pos;
 		ECS::Vector2<unsigned> _size;
+        irr::gui::IGUIEnvironment* _guienv;
+        irr::gui::IGUIButton* _button;
 	};
 } // namespace Irrlicht
 
